7f81cc00e8de73a14baeb5ffda042bb4b2dd6f21
Mehmet Hafif <32527467+mehmethafif@users.noreply.github.com>
2021-02-28 16:39:10 +0300

    edit

--- agent-setup.sh
+++ agent-setup.sh
@@ -1,39 +1,46 @@
 #! /bin/bash
 GREEN='\033[0;32m'
 NC='\033[0m'
-function install-tools {
+FAIL=0
+install-tools() {
 	echo -e "${GREEN}## apt-get update ${NC}"
-	sudo apt-get update
+	sudo apt-get update || FAIL=1
 	echo -e "${GREEN}## installing python ${NC}"
-	sudo apt-get install python -y
+	sudo apt-get install python -y || FAIL=1
 	echo -e "${GREEN}## installing python3-pip ${NC}"
-	sudo apt install python3-pip -y
+	sudo apt install python3-pip -y || FAIL=1
 	echo -e "${GREEN}## installing python requests ${NC}"
-	pip3 install requests
+	pip3 install requests || FAIL=1
 	echo -e "${GREEN}## installing git ${NC}"
-	sudo apt-get install git -y
+	sudo apt-get install git -y || FAIL=1
 	echo -e "${GREEN}## installing GoldenEye ${NC}"
-	git clone https://github.com/jseidl/GoldenEye.git
+	git clone https://github.com/jseidl/GoldenEye.git || FAIL=1
 	echo -e "${GREEN}## installing hping3 ${NC}"
-	sudo apt-get install hping3 -y
+	sudo apt-get install hping3 -y || FAIL=1
 	echo -e "${GREEN}## installing slowhttptest ${NC}"
-	sudo apt-get install slowhttptest -y
+	sudo apt-get install slowhttptest -y || FAIL=1
 	echo -e "${GREEN}## installing mz ${NC}"
-	sudo apt-get install mz -y
+	sudo apt-get install mz -y || FAIL=1
 }
 
-function install-metricbeat {
+install-metricbeat() {
 	echo -e "${GREEN}## downloading metricbeat ${NC}"
-	curl -L -O https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-7.11.1-amd64.deb
+	curl -L -O https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-7.11.1-amd64.deb || FAIL=1
 	echo -e "${GREEN}## installing metricbeat ${NC}"
-	sudo dpkg -i metricbeat-7.11.1-amd64.deb
+	sudo dpkg -i metricbeat-7.11.1-amd64.deb || FAIL=1
 	echo -e "${GREEN}## downloading metricbeat configuration file ${NC}"
-	wget <config-url> -P /etc/metricbeat/
+	wget https://raw.githubusercontent.com/mehmethafif/dumb/main/metricbeat.yml -P /etc/metricbeat/ || FAIL=1
 	echo -e "${GREEN}## starting metricbeat service ${NC}"
-	sudo systemctl start metricbeat
-	sudo systemctl enable metricbeat
+	sudo systemctl start metricbeat || FAIL=1
+	sudo systemctl enable metricbeat || FAIL=1
 }
 
 install-tools
 install-metricbeat
 
+if [ $FAIL -eq 0 ]
+then
+	touch setup-success
+else
+	touch setup-fail
+fi
